e6ed08f6166ebbd11c87196966e7d5c2b143a6b4
Prateek Srivastava <f2prateek@gmail.com>
2014-04-08 23:34:03 -0600

    Use port from system variables

--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from foursquare import Foursquare
 from map import Map
 import json
+import os
 
 app = Flask(__name__)
 app.secret_key = "ZjBjNTUzNWIyMmZmNDRkZGVmYzU5NDY2"
@@ -121,5 +122,6 @@ def debug(msg):
     app.logger.debug(str(msg))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     # app.run(debug=True)
